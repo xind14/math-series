5f4a28ad118604fd0c61100f3c75d7ee3ef69289
Xin Deng <xind1470@gmail.com>
2024-01-09 17:20:43 -0800

    added five fibonaci test structures

---
+++ math_package/series.py
@@ -0,0 +1,9 @@
+def fibonacci(n):
+  if n 
+  return str(n)
+
+
+# The function should return the nth value in the fibonacci and lucas series. You may implement the function using recursion or iteration. 
+# Both the fibonacci series and the lucas numbers are based on an identical formula. Add a third function called sum_series with one required parameter and two optional parameters. The required parameter will determine which element in the series to print. The two optional parameters will have default values of 0 and 1 and will determine the first two values for the series to be produced.
+
+# Calling this function with no optional parameters will produce numbers from the fibonacci series. Calling it with the optional arguments 2 and 1 will produce values from the lucas numbers. Other values for the optional parameters will produce other series. Again, you may use recursion or iteration, or both. Again, ensure that your function has a well-formed docstring.
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,9 +1,30 @@
-from math_package.series import fibonaci
-from math_package.series import lucas
-from math_package.series import sum_series
+from math_package.series import fibonacci
+# from math_package.series import lucas
+# from math_package.series import sum_series
 
 
 
+def test_fibonacci_one():
+  actual = fibonacci(1)
+  expected = '1'
+  assert actual == expected 
 
+def test_fibonacci_two():
+  actual = fibonacci(2)
+  expected = '2'
+  assert actual == expected 
 
+def test_fibonacci_three():
+  actual = fibonacci(3)
+  expected = '3'
+  assert actual == expected 
 
+def test_fibonacci_four():
+  actual = fibonacci(4)
+  expected = '4'
+  assert actual == expected 
+
+def test_fibonacci_five():
+  actual = fibonacci(5)
+  expected = '5'
+  assert actual == expected 
