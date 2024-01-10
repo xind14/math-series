bda14a38762c37317f979ea3564b87c995b6a88e
Xin Deng <xind1470@gmail.com>
2024-01-09 17:37:13 -0800

    fibonacci function passes all five tests

--- math_package/series.py
+++ math_package/series.py
@@ -1,6 +1,15 @@
 def fibonacci(n):
-  if n >= 0 
-    return str(n-1)
+  if n == 0:
+    return 0
+  elif n == 1:
+    return 1
+  else:
+    return fibonacci(n-1) + fibonacci (n-2)
+
+
+
+
+
 
 
 # The function should return the nth value in the fibonacci and lucas series. You may implement the function using recursion or iteration. 
--- tests/test_series.py
+++ tests/test_series.py
@@ -6,25 +6,27 @@ from math_package.series import fibonacci
 
 def test_fibonacci_zero():
   actual = fibonacci(0)
-  expected = '0'
+  expected = 0
   assert actual == expected 
 
 def test_fibonacci_one():
   actual = fibonacci(1)
-  expected = '1'
+  expected = 1
   assert actual == expected 
 
 def test_fibonacci_two():
   actual = fibonacci(2)
-  expected = '1'
+  expected = 1
   assert actual == expected 
 
 def test_fibonacci_three():
   actual = fibonacci(3)
-  expected = '2'
+  expected = 2
   assert actual == expected 
 
 def test_fibonacci_nine():
   actual = fibonacci(9)
-  expected = '21'
+  expected = 34
   assert actual == expected 
+
+  
